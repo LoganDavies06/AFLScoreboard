4839520e55886f661187cfb7e369666c9fa7b25e
Logan Davies <loganbd06@gmail.com>
2025-09-04 15:08:15 +0800

    Added main window refresh loop

--- AFLScoreboard/main.cpp
+++ AFLScoreboard/main.cpp
@@ -13,8 +13,8 @@
 #include"LButton.hpp"
 
 struct Window {
-    SDL_Window* window;
-    SDL_Renderer* renderer;
+    SDL_Window* window{ nullptr };
+    SDL_Renderer* renderer{ nullptr };
 };
 
 //sets up the window and renderer
@@ -26,6 +26,7 @@ bool init(struct screenDimensions dim, struct Window* wn) {
         success = false;
     }
     else {
+        //create window
         if (!SDL_CreateWindowAndRenderer("Scoreboard", dim.width, dim.height, 0, &wn->window, &wn->renderer)) {
             SDL_Log("Unable to create window. Error: %s\n", SDL_GetError());
             success = false;
@@ -44,7 +45,38 @@ bool init(struct screenDimensions dim, struct Window* wn) {
 int main(int argc, char* args[]) {
     int exitCode{ 0 };
 
+    constexpr screenDimensions screenDim{ 640, 480 };
+    Window window;
 
+    //initialize window
+    if (!init(screenDim, &window)) {
+        SDL_Log("Unable to initialize program");
+        exitCode = 1;
+    }
+
+    if (exitCode == 0) {
+        bool quit{ false };
+
+        //events
+        SDL_Event e;
+        SDL_zero(e);
+
+        //main loop
+        while (!quit) {
+            while (SDL_PollEvent(&e)) {
+                if (e.type == SDL_EVENT_QUIT) {
+                    quit = true;
+                }
+            }
+
+            //set window background colour
+            SDL_SetRenderDrawColor(window.renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+            SDL_RenderClear(window.renderer);
+
+            //refresh screen
+            SDL_RenderPresent(window.renderer);
+        }
+    }
 
     return exitCode;
 }
