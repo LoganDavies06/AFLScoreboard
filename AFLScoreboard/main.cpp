9c8458ba1740aa03e377bf2ddebfdc897c11dc4b
Logan Davies <loganbd06@gmail.com>
2025-09-04 14:58:22 +0800

    Added SDL initialisation

--- AFLScoreboard/main.cpp
+++ AFLScoreboard/main.cpp
@@ -12,8 +12,39 @@
 #include"LRect.hpp"
 #include"LButton.hpp"
 
+struct Window {
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+};
+
+//sets up the window and renderer
+bool init(struct screenDimensions dim, struct Window* wn) {
+    bool success{ true };
+
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("SDL initilaisation was unsuccesful. Error: %s\n", SDL_GetError());
+        success = false;
+    }
+    else {
+        if (!SDL_CreateWindowAndRenderer("Scoreboard", dim.width, dim.height, 0, &wn->window, &wn->renderer)) {
+            SDL_Log("Unable to create window. Error: %s\n", SDL_GetError());
+            success = false;
+        }
+        else {
+            if (!TTF_Init()) {
+                SDL_Log("SDL font rendering failed to load. Error: %s\n", SDL_GetError());
+                success = false;
+            }
+        }
+    }
+
+    return success;
+}
+
 int main(int argc, char* args[]) {
     int exitCode{ 0 };
+
+
 
     return exitCode;
 }
